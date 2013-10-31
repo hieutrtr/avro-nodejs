208a9611ea0bad005cfc473dca509b703b7e7fc5
Benjamin Metzger <benjamin_metzger@gensler.com>
2013-10-31 12:55:54 -0500

    adding test to the AvroCallbacks to ensure that no error is thrown with the close callback when no callback is set

--- tests/AvroCallbacks.js
+++ tests/AvroCallbacks.js
@@ -1,15 +1,25 @@
 var fs = require('fs');
 var addon = require('../build/Release/avro');
 
-var avro = new addon.Avro();
-
 
 describe("An Async test for Avro", function(){
-	it("should emit an event when the avro object has been closed", function(done){
-		avro.onclose = function(){
-			expect(true).toEqual(true);
-			done();
-		}
-		avro.close();
-	});
+	it("should emit an event when the avro object has been closed",
+    function(done){
+      var avro = new addon.Avro();
+	  	avro.onclose = function(){
+	  		expect(true).toEqual(true);
+	  		done();
+	  	}
+	  	avro.close();
+	  }
+  );
+
+  it("should not throw an error if no callback for close is defined",
+    function(done){
+      var avro = new addon.Avro();
+      avro.close();
+      expect(true).toEqual(true);
+      done();
+    }
+  );
 });
